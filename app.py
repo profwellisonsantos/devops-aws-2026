df896075b9e9ea718c9bd06556b6cf3476e33c9a
profwellisonsantos <wellison.santos@prof.infnet.edu.br>
2026-06-08 22:29:34 +0000

    :feat: mudando id

--- app.py
+++ app.py
@@ -14,5 +14,11 @@ def bemvindo():
     count = cache.incr('hits')
     return f'Olá! Esta página foi vista {count} vezes.'
 
+@app.route('/adduser')
+def adduser():
+    count = cache.incr('hits')
+    return f'Olá! Esta página foi vista {count} vezes.'
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
